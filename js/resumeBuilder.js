acf84fe070ecc266c56be9308e055a367dbb5bb8
productscientista <morin.claire12@gmail.com>
2016-07-07 11:45:08 +0200

    Name and Role
    Add Name and Role in the header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,14 @@
 /*
-
- */
  $("#main").append("Claire Morin");
  var firstName = "james"
  var age = 32;
  console.log(firstName);
+ var awesomeThoughts = "I am Claire and I am AWESOME !";
+ console.log(awesomeThoughts);
+ var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+ $("#main").append(funThoughts);
+ */
+ var formattedName = HTMLheaderName.replace("%data%","Claire Morin");
+ var formattedRole = HTMLheaderRole.replace("%data%","Product Owner / Agile Coach");
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
